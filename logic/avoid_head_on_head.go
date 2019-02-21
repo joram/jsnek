a766865ed244b1be3f5396109e0c00f8206d9cf6
John <john@sendwithus.com>
2019-02-21 11:21:53 -0800

    avoid head-on-head fix

--- logic/avoid_head_on_head.go
+++ logic/avoid_head_on_head.go
@@ -12,21 +12,24 @@ func (ge AvoidHeadOnHead) Taunt() string {
 }
 
 func (ge AvoidHeadOnHead) Decision(sr *api.SnakeRequest) int {
+
+	dangerCoords := []api.Coord{}
+	for _, snake := range sr.Board.Snakes {
+		if snake.ID != sr.You.ID && len(snake.Body) >= len(sr.You.Body){
+			dangerCoords = append(dangerCoords, snake.Head().Adjacent()...)
+		}
+	}
+
 	couldBeEaten := false
 	safeDirections := []string{}
 	avoidSquares := []api.Coord{}
-	for _, snake := range sr.Board.Snakes {
-		if snake.ID != sr.You.ID && len(snake.Body) >= len(sr.You.Body){
-			dangerCoords := snake.Head().Adjacent()
-			avoidSquares = append(avoidSquares, dangerCoords...)
-			for dir, coord := range sr.You.Head().AdjacentMap() {
-				for _, dangerCoord := range dangerCoords {
-					if coord.Equal(dangerCoord) {
-						couldBeEaten = true
-					} else {
-						safeDirections = append(safeDirections, dir)
-					}
-				}
+	avoidSquares = append(avoidSquares, dangerCoords...)
+	for dir, coord := range sr.You.Head().AdjacentMap() {
+		for _, dangerCoord := range dangerCoords {
+			if coord.Equal(dangerCoord) {
+				couldBeEaten = true
+			} else {
+				safeDirections = append(safeDirections, dir)
 			}
 		}
 	}
