6aeb2d1fa1e779600322aab9cb07d6bbab0e37ac
John Oram <john@oram.ca>
2019-12-11 21:16:41 -0800

    always move to biggest area

--- logic/go_more_room.go
+++ logic/go_more_room.go
@@ -17,29 +17,11 @@ func (ge GoMoreRoom) Decision(sr *api.SnakeRequest) int {
 
 	biggest := -1
 	biggestDir := ""
-	dirCounts := 0
-	sumRoomAvail := 0
 	for dir, count := range sr.Board.AbleToVisitCount {
-		dirCounts += 1
-		sumRoomAvail += count
 		if count > biggest {
 			biggest = count
 			biggestDir = dir
 		}
 	}
-	avgRoomAvail := sumRoomAvail / dirCounts
-
-	if float32(biggest)/float32(avgRoomAvail) > ge.Ratio {
-		d := map[string]int{
-			"up":    api.UP,
-			"down":  api.DOWN,
-			"left":  api.LEFT,
-			"right": api.RIGHT,
-			"WFT!":  api.UNKNOWN,
-		}[biggestDir]
-
-		return d
-	}
-
-	return api.UNKNOWN
+	return api.StringToDir(biggestDir)
 }
